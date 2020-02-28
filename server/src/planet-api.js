bea849c2f2d0f9ded77add83eb75d307c4c58db5
Ujjaini Mukherjee <ujjaini@Angshumans-MacBook-Pro.local>
2020-02-28 15:02:11 -0700

    Updating the server call to swapi api to fetch "terrain" values also

--- server/src/planet-api.js
+++ server/src/planet-api.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 async function get(planetId) {
   try {
     const res = await axios.get(`https://swapi.co/api/planets/${planetId}/`)
-
-    return { id: planetId, name: res.data.name }
+    console.log(res.data.terrain)
+    return { id: planetId, name: res.data.name, terrain: res.data.terrain }
   } catch (err) {
     console.log(`Error finding planet '${planetId}' from swapi`, err)
 
--- server/src/planet-cache.js
+++ server/src/planet-cache.js
@@ -2,6 +2,10 @@ const planetDb = require('./planet-db')
 const planetApi = require('./planet-api')
 
 async function get(planetId) {
+  
+  // by commenting the following lines 
+  // we can bypass data fetch from db and
+  // get data from backend API call
   const result = await planetDb.get(planetId)
 
   if (result)
