2786ab3648c9d36a31f4515b0d386e6c26263b1b
Ujjaini Mukherjee <ujjaini@Angshumans-MacBook-Pro.local>
2020-02-28 15:08:31 -0700

    Updating the database to add extra field "terrain"

--- server/src/planet-db.js
+++ server/src/planet-db.js
@@ -1,26 +1,27 @@
 const db = require('./sqlite-wrapper')
 
 async function createTable() {
-  const statement = `
+  const createTableStatement = `
     create table if not exists planets (
       id text primary key,
-      name text not null
+      name text not null,
+      terrain text
     );`
 
-  return db.execute(statement)
+  return db.execute(createTableStatement)
 }
 
 async function get(planetId) {
-  return db.get('select id, name from planets where id = $id;', { $id: planetId })
+  return db.get('select id, name, terrain from planets where id = $id;', { $id: planetId })
 }
 
 async function upsert(planet) {
   const statement = `
-    insert into planets (id, name)
-    values ($id, $name)
+    insert into planets (id, name, terrain)
+    values ($id, $name, $terrain)
     on conflict(id) do update set name = $name;`
 
-  return db.execute(statement, { $id: planet.id, $name: planet.name })
+  return db.execute(statement, { $id: planet.id, $name: planet.name, $terrain: planet.terrain })
 }
 
 module.exports = {
